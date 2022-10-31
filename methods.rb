466e0f51195a0db578823295ae9937200339490a
Adam Weaver <adamweaver90@gmail.com>
2022-10-31 12:07:36 -0400

    commit tree f47748fe0a9097f547e6db46cbbd3b7f468d1b52

--- methods.rb
+++ methods.rb
@@ -1,1 +1,22 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(girl = "Naureen")
+    puts "Hello, #{girl}!"
+end
+
+greet
+
+def greet_with_default(default = "programmer")
+    puts "Hello, #{default}!"
+end
+greet_with_default
+
+def add(num1, num2)
+    puts num1 + num2
+end
+num1 = 2
+num2 = 5
+add
